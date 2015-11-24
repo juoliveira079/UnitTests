19afbe4ad01b435a4c9d1d1028a9ad33dde6c5b7
Juliana Oliveira <julianaoliveira079@gmail.com>
2015-11-24 11:43:08 -0200

    + unit test

---
+++ Client/TestUnitNsubstitute/TestTotalizadorConta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Client.Models;
+using NSubstitute;
+
+namespace TestUnitNsubstitute
+{
+    [TestClass]
+    public class TestTotalizadorConta
+    {
+     
+        [TestMethod]
+        public void Total_Contas_Nsubstitute()
+        {
+            //Arrange
+            var conta = Substitute.For<Conta>();
+            var totalizador = Substitute.For<TotalizadorConta>();
+
+            //Act
+            double total = totalizador.ValorTotal + conta.saldo;
+            totalizador.Soma(conta);
+
+            //Assert
+
+            Assert.AreEqual(totalizador.ValorTotal, total);
+        }
+    }
+}
